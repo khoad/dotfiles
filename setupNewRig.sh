83d0cf4bafd90dd61c3394c5c266a20a9a5b1d49
Khoa Nguyen <kh.dang.nguyen@gmail.com>
2015-12-10 22:47:34 -0800

    Fixed typos, download iTerm fonts and colors

--- setupNewRig.sh
+++ setupNewRig.sh
@@ -1,6 +1,5 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install wget
 brew install git
 brew install node
 brew install zsh
@@ -10,7 +9,7 @@ brew tap caskroom/cask
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install shiftit
-brew cask install earabiner
+brew cask install karabiner
 brew cask install alfred
 brew cask install sourcetree
 brew cask install atom
@@ -18,6 +17,14 @@ brew cask install atom
 apm install atom-solarized-dark-ui
 apm install monokai
 apm install atom-beautify
+apm install sublime-style-column-selection
 
+# iTerm setup
+# Have to change font in iTerm manually :(
+curl -O https://raw.githubusercontent.com/powerline/fonts/master/Meslo/Meslo%20LG%20M%20DZ%20Regular%20for%20Powerline.otf
+mv Meslo%20LG%20M%20DZ%20Regular%20for%20Powerline.otf /Library/Fonts
+# Have to load colors manually :(
+curl -O https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Solarized%20Dark%20Higher%20Contrast.itermcolors
+
+# install oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-wget https://github.com/powerline/fonts/blob/master/Meslo/Meslo%20LG%20M%20DZ%20Regular%20for%20Powerline.otf
